decbd189bd1a9db4a67fd0de06b2bcbec0db792c
Peter Jacobson <peterj812@gmail.com>
2022-03-26 15:40:27 -0700

    My 2022 Hackathon submission!

--- buddy/src/main/java/com/buddy/App.java
+++ buddy/src/main/java/com/buddy/App.java
@@ -11,15 +11,17 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text; 
+import javafx.scene.text.Text;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -32,7 +34,7 @@ public class App extends Application {
     private static Scene scene;
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws FileNotFoundException {
         // Creates scene grid and pane title
         primaryStage.setTitle("Contractor Buddy");
         GridPane grid = new GridPane();
@@ -63,7 +65,11 @@ public class App extends Application {
         // Add desired rows for the form
         rowNames.add("Square Footage"); rowNames.add("Hip"); rowNames.add("Ridge"); rowNames.add("Permitter"); 
         rowNames.add("Pipes/Vents"); rowNames.add("Skylights"); rowNames.add("Chimmenys"); 
-        rowNames.add("1/4in Flashing"); rowNames.add("1/2in Flashing"); rowNames.add("1in flashing "); 
+        rowNames.add("1.5in Flashing"); rowNames.add("2in Flashing"); rowNames.add("3in flashing "); 
+        /* For V2
+        rowNames.add("Customer Name"); rowNames.add("Customer Address"); rowNames.add("Customer Email");
+        rowNames.add("Customer Phone");
+        */
 
         // Loop throguh the row names and creates a form row for each
         for (int i = 0; i < rowNames.size(); i++) {
@@ -148,38 +154,136 @@ public class App extends Application {
         scenetitle.setFont(Font.font("Tahoma", FontWeight.BOLD, 20));
         grid2.add(scenetitle, 0, 0, 2, 1);
 
+        /* 
+        Job Cost Field Generation - Add labels as necessary
+        *See Avalon Roofing Blankl Pricing Sheet for Pricing - Must be requested to see*
+        **For future releases - there has to be a way to improve how this is done, no time for that currently**
+        */
+        // Tracks total cost
+        double TotalCost = 0;
+
         // Displays Total square footage
-        int squareFootage = data.get(0);
+        double squareFootage = data.get(0);
         Label squareFootageL = new Label("Total Square Footage: " + squareFootage);
         squareFootageL.setFont(Font.font("Tahoma", FontWeight.NORMAL, 12));
-        grid2.add(squareFootageL, 0, 3);
+        grid2.add(squareFootageL, 0, 2);
+
+        // Displays permitter
+        double permitter = data.get(3);
+        Label permitterL = new Label("Permitter Length: " + permitter);
+        permitterL.setFont(Font.font("Tahoma", FontWeight.NORMAL, 12));
+        grid2.add(permitterL, 0, 3);
+
+        // Displays number of vents
+        double vents = data.get(4);
+        Label ventsL = new Label("Total Vents: " + vents);
+        ventsL.setFont(Font.font("Tahoma", FontWeight.NORMAL, 12));
+        grid2.add(ventsL, 0, 4);
+
+        // Displays number of skylights and chimmenys
+        double SkyChim = data.get(5) + data.get(6);
+        Label SkyChimL = new Label("Total Skylights and Chimmenys: " + SkyChim);
+        SkyChimL.setFont(Font.font("Tahoma", FontWeight.NORMAL, 12));
+        grid2.add(SkyChimL, 0, 5);
 
         // Caluates and displays numnber of squares
         double numSquares = squareFootage / 100;
         Label numSquaresL = new Label("Number of Squares: " + numSquares); 
         numSquaresL.setFont(Font.font("Tahoma", FontWeight.NORMAL, 12));
-        grid2.add(numSquaresL, 0, 4);
+        grid2.add(numSquaresL, 0, 6);
 
-         // Caluates and displays cost of plywood by square
-         double costForPlywood = numSquares * 110;
-         Label costSquaresL = new Label("Plywood Cost: " + costForPlywood); 
-         costSquaresL.setFont(Font.font("Tahoma", FontWeight.NORMAL, 12));
-         grid2.add(costSquaresL, 0, 5);
+        // Displays hip length (They don't lie...)
+        double HipLength = data.get(1);
+        Label HipLengthL = new Label("Hip Length: " + HipLength); 
+        HipLengthL.setFont(Font.font("Tahoma", FontWeight.NORMAL, 12));
+        grid2.add(HipLengthL, 0, 7);
 
-        
+        // Displays ridge length
+        double RidgeLength = data.get(2);
+        Label RidgeLengthL = new Label("Ridge Length: " + RidgeLength); 
+        RidgeLengthL.setFont(Font.font("Tahoma", FontWeight.NORMAL, 12));
+        grid2.add(RidgeLengthL, 0, 8);
 
+        // Calcualtes and displays cost for ridge shingles, adds to total
+        double RidgeShinglesCost = RidgeLength/20 * 47.30;
+        TotalCost += RidgeShinglesCost;
+        Label RidgeShinglesCostL = new Label("Ridge Shingles Cost: $" + RidgeShinglesCost); 
+        RidgeShinglesCostL.setFont(Font.font("Tahoma", FontWeight.NORMAL, 12));
+        grid2.add(RidgeShinglesCostL, 0, 9);
+
+        // Caluates and displays cost of plywood by square, adds to total
+        double PlywoodCost = numSquares * 70;
+        TotalCost += PlywoodCost;
+        Label PlywoodCostL = new Label("Plywood Cost: $" + PlywoodCost); 
+        PlywoodCostL.setFont(Font.font("Tahoma", FontWeight.NORMAL, 12));
+        grid2.add(PlywoodCostL, 0, 10);
+
+        // Caluates and displays cost of Shingles by square, adds to total
+        double ShinglesCost = numSquares * 110;
+        TotalCost += ShinglesCost;
+        Label ShinglesCostL = new Label("Shingles Cost: $" + ShinglesCost); 
+        ShinglesCostL.setFont(Font.font("Tahoma", FontWeight.NORMAL, 12));
+        grid2.add(ShinglesCostL, 0, 11);
+
+        // Caluates and displays cost of felt by square, adds to total
+        double FeltCost = numSquares/2 * 32.25;
+        TotalCost += FeltCost;
+        Label FeltCostL = new Label("Felt Cost: $" + FeltCost); 
+        FeltCostL.setFont(Font.font("Tahoma", FontWeight.NORMAL, 12));
+        grid2.add(FeltCostL, 0, 12);
+
+        // Caluates and displays cost of 1.5in flashing, adds to total
+        double flashing1_5inCost = numSquares * 9.96;
+        TotalCost += flashing1_5inCost;
+        Label flashing1_5inCostL = new Label("1.5in Flashing Cost: $" + flashing1_5inCost); 
+        flashing1_5inCostL.setFont(Font.font("Tahoma", FontWeight.NORMAL, 12));
+        grid2.add(flashing1_5inCostL, 0, 13);
+
+        // Caluates and displays cost of 2in flashing, adds to total
+        double flashing2inCost = numSquares * 9.96;
+        TotalCost += flashing2inCost;
+        Label flashing2inCostL = new Label("2in Flashing Cost: $" + flashing2inCost); 
+        flashing2inCostL.setFont(Font.font("Tahoma", FontWeight.NORMAL, 12));
+        grid2.add(flashing2inCostL, 0, 14);
+
+        // Caluates and displays cost of 3in flashing, adds to total
+        double flashing3inCost = numSquares * 9.96;
+        TotalCost += flashing3inCost;
+        Label flashing3inCostL = new Label("3in Flashing Cost: $" + flashing3inCost); 
+        flashing3inCostL.setFont(Font.font("Tahoma", FontWeight.NORMAL, 12));
+        grid2.add(flashing3inCostL, 0, 15);
+
+        // Caluates and displays cost of torch down, adds to total
+        double torchDownCost = numSquares * 106;
+        TotalCost += torchDownCost;
+        Label torchDownCostL = new Label("Torch Down Cost: $" + torchDownCost); 
+        torchDownCostL.setFont(Font.font("Tahoma", FontWeight.NORMAL, 12));
+        grid2.add(torchDownCostL, 0, 16);
+
+        // Caluates and displays cost of propane, adds to total
+        double propaneCost = numSquares * 106;
+        TotalCost += propaneCost;
+        Label propaneCostL = new Label("Propane Cost: $" + propaneCost); 
+        propaneCostL.setFont(Font.font("Tahoma", FontWeight.NORMAL, 12));
+        grid2.add(propaneCostL, 0, 17);
+
+        // Calcualtes and displays total cost
+        Label TotalCostL = new Label("Total: $" + TotalCost); 
+        TotalCostL.setFont(Font.font("Tahoma", FontWeight.BOLD, 14));
+        grid2.add(TotalCostL, 0, 18);
 
         // Sets scene size, title, and displays the scene
-		Scene jobCostScene = new Scene(grid2, 300, 450);
+		Scene jobCostScene = new Scene(grid2, 300, 500);
         jobCostStage.setScene(jobCostScene);
         jobCostStage.setTitle("Job Cost Report");
 		jobCostStage.show(); 
     }
 
-    // Note: There is a lot of "junk" code that is an artifcate of creating a maven project and is unused
+/* ----------------------------------------- End of Meaningful Code ---------------------------------------------------- */
+    // Note: There is a lot of "junk" code that is an artifcate of creating a maven project
+    //         below this note, and in the other files. Some is in use, most is not.
 
-
-    // Below is Autogenerated, does stuff to run, forsure does smth...yup
+    // Below is Autogenerated, does stuff to run, forsure does smth...yup 
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
